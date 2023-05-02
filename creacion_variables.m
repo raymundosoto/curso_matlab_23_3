a8b8b02e12c2444ce2e97ec619ecbe79d0a17dd2
Raymundo Soto <72757419+raymundosoto@users.noreply.github.com>
2023-05-02 17:13:30 -0600

    Operaciones básicas

---
+++ creacion_variables.m
@@ -0,0 +1,33 @@
+clc
+clear
+clear all
+close all
+
+x = 3.1;
+y = -2.5;
+% Estas son las operaciones básicas
+suma = x + y;
+resta = x - y;
+multiplicacion = x*y;   % Multiplicación
+division = x / y;
+potencia = x^y ;
+logaritmo_natural = log(x);
+log_10 = log10(x);
+exponencial = exp(y);
+cos(x);   % Coseno del ángulo en radianes
+cosd(x);  % Coseno del ángulo en grados
+sin(x);    %seno del angulo
+sind(x);    %seno del angulo
+
+%% Operaciones con números complejos
+x = 3 - 2i; %Numero complejo
+y = -2.1 + 3.5i; %Numero complejo
+
+x + y; %suma de numeros complejos
+abs(x); %Magnitud o modulo del numero complejo
+
+magnitud = sqrt(real(x)^2 + imag(x)^2);
+angule = angle(x); %angulo del numero complejo
+
+
+
